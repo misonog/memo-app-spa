ce72d87f38efa87bdf3f15d60a2eb89aedaf4ee0
misono <ginpeimisonou@gmail.com>
2021-11-07 17:08:31 +0900

    mutationsの雛形作成

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,24 @@
 import { createStore } from 'vuex'
+import * as types from './mutation-type'
 
 export default createStore({
   state: {
   },
   mutations: {
+    [types.ADD_MEMO] (state, payload) {
+      // TODO:
+      throw new Error('ADD_MEMO mutations should be implemented')
+    },
+
+    [types.UPDATE_MEMO] (state, payload) {
+      // TODO:
+      throw new Error('UPDATE_MEMO mutations should be implemented')
+    },
+
+    [types.REMOVE_MEMO] (state, payload) {
+      // TODO:
+      throw new Error('REMOVE_MEMO mutations should be implemented')
+    }
   },
   actions: {
   },
---
+++ src/store/mutation-type.js
@@ -0,0 +1,3 @@
+export const ADD_MEMO = 'ADD_MEMO'
+export const UPDATE_MEMO = 'UPDATE_MEMO'
+export const REMOVE_MEMO = 'REMOVE_MEMO'
