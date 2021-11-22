69cefebb83f1f67da94cc19e52268173a5fbfb03
misono <ginpeimisonou@gmail.com>
2021-11-22 13:41:19 +0900

    ヘッダーの追加

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
-  <div id="nav">
+  <div class="container">
+    <Header></Header>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: { Header }
+}
+</script>
 
 <style>
 #app {
@@ -11,18 +21,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="navbar navbar-light">
+    <router-link :to="{ name: 'memoList' }" class="navbar-brand mb-0 h1">MemoApp</router-link>
+  </header>
+</template>
